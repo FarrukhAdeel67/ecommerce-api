4be52434ffc45371ace79a36c0169d8905eb69fa
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-09-26 09:28:02 -0700

    Create 20220926130406-create-user_shopes-table.js

---
+++ database/migrations/20220926130406-create-user_shopes-table.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { DataTypes } = require("sequelize");
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("user_shopes",{
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    shopkeeper_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    shop_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    fk_user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    updatedAt: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    })
+  },
+
+  down: async(queryInterface, Sequelize) => {
+    await queryInterface.dropTable("user_shopes");
+  }
+};
