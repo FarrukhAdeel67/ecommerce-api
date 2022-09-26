8ff34fcea6fc6ab0bb2d267b0d4ef41d115c7a6a
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-26 16:30:10 +0500

    verify_otp is added

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
-const { random } = require("lodash");
 const { Users, sequelize, UserOtps } = require("../models");
-
+const moment = require("moment");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
 module.exports = {
   signUp: async (req, res) => {
     const transaction = await sequelize.transaction();
@@ -31,16 +32,54 @@ module.exports = {
           otp,
           fk_user_id: user.id,
         },
-        { transaction }
+        { individualHooks: true, transaction }
       );
       await transaction.commit();
       res.status(200).send({ user, userOtp });
     } catch (err) {
       console.log(err);
-      await transaction.rollBack();
+      await transaction.rollback();
       return res
         .status(err.status || 500)
-        .message(err.message || "Something went wrong");
+        .message(err.message || "Something went wrong...");
+    }
+  },
+  verifyOtp: async (req, res) => {
+    const transaction = await sequelize.transaction();
+    try {
+      let { user } = req;
+      const { otp } = req.body;
+      if (otp.toString().length != 6) {
+        throw { error: 400, message: "OTP must be a 6 digits number " };
+      }
+      const verifyOtp = await UserOtps.findOne({
+        where: {
+          otp,
+          fk_user_id: user.id,
+        },
+        order: [["createdAt", "DESC"]],
+        transaction,
+      });
+      if (moment().unix() > verifyOtp.expiry) {
+        throw { error: 403, message: "OTP is expired" };
+      }
+      if (user.phone_number_verified === false) {
+        user = await user.update(
+          {
+            phone_number_verified: true,
+          },
+          transaction
+        );
+      }
+      const token = jwt.sign({ user }, config.get("jwt_secret"));
+      await transaction.commit();
+      res.status(200).send({ token, user });
+    } catch (err) {
+      console.log(err);
+      await transaction.rollBack();
+      res
+        .status(err.status || 500)
+        .message(err.message || "Something went wrong...");
     }
   },
   get: async (req, res) => {
@@ -49,6 +88,36 @@ module.exports = {
       return res.status(200).send({ users });
     } catch (err) {
       console.log(err);
+      return res
+        .status(err.status || 500)
+        .message(err.message || "Something went wrong...");
+    }
+  },
+  update: async (req, res) => {
+    const transaction = await sequelize.transaction();
+    try {
+      const { name } = req.body;
+      const { userId } = req.params;
+      const user = await Users.findByPk(userId);
+      if (!user) {
+        throw { status: 409, message: "User doesn't exist" };
+      }
+      let updatedUser = await Users.update(
+        {
+          name: name ? name : user.name,
+        },
+        {
+          where: { id: userId },
+        },
+        { transaction }
+      );
+      await transaction.commit();
+      res.status(200).send({
+        updatedUser,
+      });
+    } catch (err) {
+      console.log(err);
+      await transaction.rollback();
       return res
         .status(err.status || 500)
         .message(err.message || "Something went wrong...");
