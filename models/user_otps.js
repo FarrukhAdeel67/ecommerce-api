579c3932d0e1c61bf853115d3336a45bf5184805
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-09-26 09:28:24 -0700

    Update user_otps.js

--- models/user_otps.js
+++ models/user_otps.js
@@ -41,3 +41,48 @@ module.exports = (sequelize, DataTypes) => {
   });
   return UserOtp;
 };
+
+
+// "use strict";
+// const moment = require("moment");
+// const user = require("../controllers/user");
+
+// module.exports = (sequelize, DataTypes)=>{
+//     const UserShop = sequelize.define("user_shopes",{
+//         id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             autoIncrement: true,
+//             primaryKey: true,
+//         },
+//         shopkeeper_name: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         shop_name: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         fk_user_id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//         },
+//         createdAt: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//         },
+//         updatedAt: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//         },
+//     })
+
+//     UserShop.beforeCreate(async(user_shopes)=>{
+//         user_shopes.createdAt = moment().unix();
+//         user_shopes.updatedAt = moment().unix();
+//     })
+//     UserShop.beforeUpdate(async(user_shopes)=>{
+//         user_shopes.updatedAt = moment().unix();
+//     })
+//     return UserShop;
+// }
