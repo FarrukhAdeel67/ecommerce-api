3ce7ee967b8930a1855dfe56f863c9920e5d2a03
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-26 16:30:49 +0500

    route updated

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,10 @@ const router = Router();
 
 // Controllers
 const controller = require("../controllers/user");
+const isUser = require("../middleware/is_user");
 
-router.use("/", controller.signUp);
+router.post("/", controller.signUp);
+router.put("/:userId", controller.update);
+router.put("/:userId/verifyOtp", isUser, controller.verifyOtp);
 
 module.exports = router;
