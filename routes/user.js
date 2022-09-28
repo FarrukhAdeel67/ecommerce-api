c6c96a6fffe940ac77d65706cff2416b56ceed8f
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-28 23:46:05 +0500

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,10 @@ const router = Router();
 const controller = require("../controllers/user");
 const isUser = require("../middleware/is_user");
 const isShop = require("../middleware/is_user");
-
+const shopController = require("./shops");
 router.post("/", controller.signUp);
 router.put("/:userId", isUser, controller.update);
 router.put("/:userId/verifyOtp", isUser, controller.verifyOtp);
-router.post("/:userId/shops", isUser, controller.shop);
-router.get("/:userId/shops/:shopId", isUser, controller.getShop);
+router.use("/:userId", isUser, shopController);
 
 module.exports = router;
