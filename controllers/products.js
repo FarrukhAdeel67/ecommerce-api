9037021e08b37f98259c770c0e95cacbc9046d8e
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-10-01 23:07:47 +0500

    create product api created

--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,24 @@
-const { sequelize , Products} = require("../models");
+const { sequelize, Products } = require("../models");
 
 module.exports = {
   postProduct: async (req, res) => {
     const transaction = await sequelize.transaction();
     try {
-      const {name, price, discription} = req.body;
-      let {shop} = req;
-      console.log(Products)
+      const { name, price, discription } = req.body;
+      let { shop } = req;
+      console.log(Products);
       console.log(shop.id);
-      const product = await Products.create({
-        name,
-        price,
-        discription,
-        fk_shop_id: shop.id,
-      },transaction);
+      const product = await Products.create(
+        {
+          name,
+          price,
+          discription,
+          fk_shop_id: shop.id,
+        },
+        transaction
+      );
       await transaction.commit();
-      res.status(200).send({product});
+      res.status(200).send({ product });
     } catch (err) {
       console.log(err);
       await transaction.rollBack();
