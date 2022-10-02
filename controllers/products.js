3b46e4cb63174d5f7ff911c4c3ee793ab9f63567
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-10-02 16:08:57 +0500

    get all the products of a specific shop

--- controllers/products.js
+++ controllers/products.js
@@ -1,25 +1,46 @@
-const { sequelize , Products} = require("../models");
+const { sequelize, Products } = require("../models");
 
 module.exports = {
   postProduct: async (req, res) => {
     const transaction = await sequelize.transaction();
     try {
-      const {name, price, discription} = req.body;
-      let {shop} = req;
-      console.log(Products)
-      console.log(shop.id);
-      const product = await Products.create({
-        name,
-        price,
-        discription,
-        fk_shop_id: shop.id,
-      },transaction);
+      const { name, price, discription } = req.body;
+      let { shop } = req;
+      const product = await Products.create(
+        {
+          name,
+          price,
+          discription,
+          fk_shop_id: shop.id,
+        },
+        transaction
+      );
       await transaction.commit();
-      res.status(200).send({product});
+      res.status(200).send({ product });
     } catch (err) {
       console.log(err);
       await transaction.rollBack();
       return res
+        .status(err.status || 500)
+        .message(err.message || "something went wrong");
+    }
+  },
+  getProducts: async (req, res) => {
+    const transaction = await sequelize.transaction();
+    try {
+      let { shop } = req;
+      const products = await Products.findAll({
+        where: {
+          fk_shop_id: shop.id,
+        },
+        transaction,
+      });
+      res.status(200).send({ products });
+      await transaction.commit();
+    } catch (err) {
+      console.log(err);
+      await transaction.rollBack();
+      res
         .status(err.status || 500)
         .message(err.message || "something went wrong");
     }
