56e77d68e02a48fc7d0b8ac68f92c98e695d1007
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-10-02 16:09:15 +0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -2,5 +2,6 @@ const { Router } = require("express");
 const router = Router();
 const controller = require("../controllers/products");
 router.post("/products", controller.postProduct);
+router.get("/products", controller.getProducts);
 
 module.exports = router;
