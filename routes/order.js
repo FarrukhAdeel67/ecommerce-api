69d80a0aa10a1a9649d92c9af49faacb81b008fb
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-10-09 07:30:34 -0700

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
 const controller = require("../controllers/orders");
+const checkProduct = require("../middleware/checkProduct");
+const isProduct = require("../middleware/is_product");
 
-router.post("/", controller.createOrder);
+router.post("/", isProduct, checkProduct, controller.createOrder);
 
 
 module.exports = router;
