beffef2d7aca65bcd77262c3bf09b7f73b953154
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-10-09 03:50:04 -0700

    Update shops.js

--- routes/shops.js
+++ routes/shops.js
@@ -3,8 +3,11 @@ const router = Router();
 const controller = require("../controllers/shops");
 const productController = require("./products");
 const isShop = require("../middleware/is_shop");
+const orderRoute = require("./order");
+
 router.post("/shops", controller.shop);
 router.get("/shops/:shopId", isShop, controller.getShop);
 router.use("/shops/:shopId", isShop, productController);
+router.use("/shops/:shopId/orders", isShop, orderRoute);
 
 module.exports = router;
