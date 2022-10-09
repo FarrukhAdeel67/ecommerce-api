3ede053612c06ecc3778bad6e2c56e63b1d64175
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-10-09 07:30:09 -0700

    Update orders.js

--- controllers/orders.js
+++ controllers/orders.js
@@ -4,16 +4,9 @@ module.exports = {
   createOrder: async (req, res) => {
     const transaction = await sequelize.transaction();
     try {
-      const {fk_product_id} = req.body;
+      const {product} = req;
       const { user } = req;
       const { shop } = req;
-      const product = await Products.findByPk(fk_product_id);
-      if (!product) {
-        throw { error: 500, message: "Products does not exist..." };
-      }
-      if (product.fk_shop_id !== shop.id) {
-        throw { status: 409, message: "Product does not belong to that shop." };
-      }
       const data = await Orders.create({
         fk_user_id: user.id,
         fk_product_id: product.id,
